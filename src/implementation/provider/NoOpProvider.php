78be67cd93719742e0e0169a6b2ff9bffe95086b
Tom Carrio <tom@carrio.dev>
2022-10-30 02:17:33 -0400

    feat: provide a base AbstractProvider for provider implementations to use

--- src/implementation/provider/NoOpProvider.php
+++ src/implementation/provider/NoOpProvider.php
@@ -4,24 +4,13 @@ declare(strict_types=1);
 
 namespace OpenFeature\implementation\provider;
 
-use OpenFeature\implementation\common\Metadata;
-use OpenFeature\interfaces\common\Metadata as MetadataInterface;
 use OpenFeature\interfaces\flags\EvaluationContext;
-use OpenFeature\interfaces\hooks\Hook;
 use OpenFeature\interfaces\provider\Provider;
 use OpenFeature\interfaces\provider\ResolutionDetails as ResolutionDetailsInterface;
-use Psr\Log\LoggerAwareTrait;
 
-class NoOpProvider implements Provider
+class NoOpProvider extends AbstractProvider implements Provider
 {
-    use LoggerAwareTrait;
-
-    private static string $NAME = 'NoOpProvider';
-
-    public function getMetadata(): MetadataInterface
-    {
-        return new Metadata(self::$NAME);
-    }
+    protected static string $NAME = 'NoOpProvider';
 
     public function resolveBooleanValue(string $flagKey, bool $defaultValue, ?EvaluationContext $context = null): ResolutionDetailsInterface
     {
@@ -49,21 +38,5 @@ class NoOpProvider implements Provider
     public function resolveObjectValue(string $flagKey, $defaultValue, ?EvaluationContext $context = null): ResolutionDetailsInterface
     {
         return ResolutionDetailsFactory::fromSuccess($defaultValue);
-    }
-
-    /**
-     * @return Hook[]
-     */
-    public function getHooks(): array
-    {
-        return [];
-    }
-
-    /**
-     * @param Hook[] $hooks
-     */
-    public function setHooks(array $hooks): void
-    {
-        // no-op
     }
 }
