6c22459ee5601d70ae630de004fc11c4da552d70
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2025-03-22 14:08:08 +0000

    refactor: added FlagValueType::tryFromResolutionDetails() method

--- src/interfaces/flags/FlagValueType.php
+++ src/interfaces/flags/FlagValueType.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace OpenFeature\interfaces\flags;
 
+use OpenFeature\interfaces\provider\ResolutionDetails;
+
+use function is_array;
+use function is_bool;
+use function is_float;
+use function is_int;
+use function is_string;
+
 enum FlagValueType: string
 {
     case String = 'STRING';
@@ -12,24 +20,28 @@ enum FlagValueType: string
     case Object = 'OBJECT';
     case Boolean = 'BOOLEAN';
 
-    /**
-     * @deprecated prefer enum value over const
-     */
-    public const STRING = 'STRING';
-    /**
-     * @deprecated prefer enum value over const
-     */
-    public const INTEGER = 'INTEGER';
-    /**
-     * @deprecated prefer enum value over const
-     */
-    public const FLOAT = 'FLOAT';
-    /**
-     * @deprecated prefer enum value over const
-     */
-    public const OBJECT = 'OBJECT';
-    /**
-     * @deprecated prefer enum value over const
-     */
-    public const BOOLEAN = 'BOOLEAN';
+    public static function tryFromResolutionDetails(ResolutionDetails $resolutionDetails): ?self
+    {
+        $value = $resolutionDetails->getValue();
+
+        return match (true) {
+            is_bool($value) => self::Boolean,
+            is_float($value) => self::Float,
+            is_int($value) => self::Integer,
+            is_string($value) => self::String,
+            is_array($value) => self::Object,
+            default => null,
+        };
+    }
+
+    /** @deprecated prefer enum value over const */
+    public const STRING = self::String;
+    /** @deprecated prefer enum value over const */
+    public const INTEGER = self::Integer;
+    /** @deprecated prefer enum value over const */
+    public const FLOAT = self::Float;
+    /** @deprecated prefer enum value over const */
+    public const OBJECT = self::Object;
+    /** @deprecated prefer enum value over const */
+    public const BOOLEAN = self::Boolean;
 }
