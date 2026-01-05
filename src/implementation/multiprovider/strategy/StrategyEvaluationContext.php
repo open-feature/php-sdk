b8d5d3bbe842eab807300a026a95f7eaff68c2b6
tmakinde <makindet74@gmail.com>
2026-01-05 07:39:05 +0100

    chore: Move invariant check for flag type vs default value up into the context
    Signed-off-by: tmakinde <makindet74@gmail.com>

--- src/implementation/multiprovider/strategy/StrategyEvaluationContext.php
+++ src/implementation/multiprovider/strategy/StrategyEvaluationContext.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace OpenFeature\implementation\multiprovider\strategy;
 
+use InvalidArgumentException;
 use OpenFeature\interfaces\flags\EvaluationContext;
+
+use function is_array;
+use function is_bool;
+use function is_float;
+use function is_int;
+use function is_string;
 
 /**
  * Context information for the overall evaluation across all providers.
@@ -17,6 +24,41 @@ class StrategyEvaluationContext
         private mixed $defaultValue,
         private EvaluationContext $evaluationContext,
     ) {
+        // Invariant check: flagType must match defaultValue type
+        switch ($flagType) {
+            case 'boolean':
+                if (!is_bool($defaultValue)) {
+                    throw new InvalidArgumentException('Default value for boolean flag must be bool');
+                }
+
+                break;
+            case 'string':
+                if (!is_string($defaultValue)) {
+                    throw new InvalidArgumentException('Default value for string flag must be string');
+                }
+
+                break;
+            case 'integer':
+                if (!is_int($defaultValue)) {
+                    throw new InvalidArgumentException('Default value for integer flag must be int');
+                }
+
+                break;
+            case 'float':
+                if (!is_float($defaultValue)) {
+                    throw new InvalidArgumentException('Default value for float flag must be float');
+                }
+
+                break;
+            case 'object':
+                if (!is_array($defaultValue)) {
+                    throw new InvalidArgumentException('Default value for object flag must be array');
+                }
+
+                break;
+            default:
+                throw new InvalidArgumentException('Unknown flag type: ' . $flagType);
+        }
     }
 
     public function getFlagKey(): string
